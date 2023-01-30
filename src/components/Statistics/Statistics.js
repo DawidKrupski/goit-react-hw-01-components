66930a48f68e5bea1b60c2bbc2ee9062b6f84ceb
DawidKrupski <dawid.krupski323@gmail.com>
2023-01-30 16:24:29 +0100

    Task #2

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -13,7 +13,6 @@ export const Statistics = ({ stats, title }) => {
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
-
       <ul className={css['stat-list']}>
         <li style={{ background: getDarkColor() }} className={css.item}>
           <span className={css.label}>.docx</span>
