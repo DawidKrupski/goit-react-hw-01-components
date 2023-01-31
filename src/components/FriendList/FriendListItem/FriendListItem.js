0276f82dcbf19f2418f0763e4e516667deb08af7
DawidKrupski <dawid.krupski323@gmail.com>
2023-01-31 21:41:53 +0100

    task #3

--- src/components/FriendList/FriendListItem/FriendListItem.js
+++ src/components/FriendList/FriendListItem/FriendListItem.js
@@ -1,6 +1,5 @@
-import React from 'react';
 import { PropTypes } from 'prop-types';
-// import css from './FriendListItem.module.css';
+import css from './FriendListItem.module.css';
 
 export const FriendListItem = ({ id, avatar, name, isOnline }) => {
   return (
